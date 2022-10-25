122bff533264ab3bd82a86cfa5bd860f7ab733e0
LGala <galafassilorenzo@gmail.com>
2022-10-26 01:01:20 +0200

    keep going

--- src/zsh.sh
+++ src/zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "${ICARO_HOME}" ]; then
+if [ "$(grep -rnw ~/.zshrc -e 'export ICARO_HOME' | wc -l)" -eq 0 ]; then
    printf "\n" >> ~/.zshrc
    echo "export ICARO_HOME=\"~/icaro\"" >> ~/.zshrc
 fi
@@ -14,7 +14,7 @@ then
    wget -P ~/icaro/cli https://github.com/icarolang/cli/releases/download/"${last_tag}"/cli-"${last_tag}".jar
 
    printf "\n" >> ~/.zshrc
-   echo "alias icaro=\"java -jar ~/icaro/cli-\"${last_tag}\".jar\"" >> ~/.zshrc
+   echo "alias icaro=\"java -jar ~/icaro/cli-""${last_tag}"".jar\"" >> ~/.zshrc
 fi
 
 
@@ -23,7 +23,7 @@ mkdir -p ~/icaro/lang
 if [ "$(find ~/icaro/lang | wc -l)" -eq 1 ]
 then
    last_tag=$(curl "https://api.github.com/repos/icarolang/lang/tags" | awk "FNR == 3 {print $3}" | tr -d ' :",' | sed s/"name"//)
-   wget -P ~/icaro/lang https://github.com/icarolang/lang/releases/download/\"${last_tag}\"/lang-\"${last_tag}\".jar
+   wget -P ~/icaro/lang https://github.com/icarolang/lang/releases/download/"${last_tag}"/lang-"${last_tag}".jar
 fi
 
 . ~/.zshrc 2> /dev/null

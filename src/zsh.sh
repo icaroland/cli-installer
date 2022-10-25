328a43890046137d3167f8becadfaadfebc95da2
LGala <galafassilorenzo@gmail.com>
2022-10-26 00:08:15 +0200

    keep going

--- src/zsh.sh
+++ src/zsh.sh
@@ -12,8 +12,7 @@ if [ "$(find ~/icaro/cli | wc -l)" -eq 0 ]
 then
    # shellcheck disable=SC2154
    lastTag=${curl "https://api.github.com/repos/icarolang/cli/tags" | awk "FNR == 3 {print $3}" | tr -d ' :",' | sed s/"name"//}
-   wget -P ~/icaro/cli
-   wget https://github.com/icarolang/cli/releases/download/"${lastTag}"/cli-"${lastTag}".jar
+   wget -P ~/icaro/cli https://github.com/icarolang/cli/releases/download/"${lastTag}"/cli-"${lastTag}".jar
 
    printf "\n" >> ~/.zshrc
    echo "alias icaro="java -jar ~/icaro/cli.jar"" >> ~/.zshrc
@@ -27,6 +26,5 @@ mkdir -p ~/icaro/lang
 if [ "$(find ~/icaro/lang | wc -l)" -eq 0 ]
 then
    lastTag=${curl "https://api.github.com/repos/icarolang/lang/tags" | awk "FNR == 3 {print $3}" | tr -d ' :",' | sed s/"name"//}
-   wget -P ~/icaro/lang
-   wget https://github.com/icarolang/lang/releases/download/"${lastTag}"/lang-"${lastTag}".jar
+   wget -P ~/icaro/lang https://github.com/icarolang/lang/releases/download/"${lastTag}"/lang-"${lastTag}".jar
 fi

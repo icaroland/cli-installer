757cae7d5445131292b53615ea66f20938ab5e8c
LGala <galafassilorenzo@gmail.com>
2022-11-28 22:00:08 +0100

    keep going

--- test/macos.sh
+++ test/macos.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 touch ~/.zshrc
-#sh src/unix/installer.sh
-
-curl -s "https://api.github.com/repos/icaroland/cli-entrypoint/tags" | awk "FNR == 3 {print $3}" | tr -d ' :",' | sed s/"name"//
+sh src/unix/installer.sh
 
 icaro help

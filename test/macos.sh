a164c05f5a6491bb5eb2d07ee680fe94da1715de
LGala <galafassilorenzo@gmail.com>
2022-11-28 22:43:54 +0100

    keep going

--- test/macos.sh
+++ test/macos.sh
@@ -2,7 +2,7 @@
 touch ~/.zshrc
 
 last_cli_entrypoint_tag=$(curl "https://api.github.com/repos/icaroland/cli-entrypoint/tags" | awk "FNR == 3 {print $3}" | tr -d ' :",' | sed s/"name"//)
-echo $last_cli_entrypoint_tag
+echo "$last_cli_entrypoint_tag"
 #wget ~/icaro/cli https://github.com/icaroland/cli-entrypoint/releases/download/"${last_cli_entrypoint_tag}"/entrypoint.jar
 #ls ~/icaro/cli
 #sh src/unix/installer.sh

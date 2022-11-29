e4834ce13fd8e6e0663ee02b7fd57dd4162769c4
LGala <galafassilorenzo@gmail.com>
2022-11-29 23:23:12 +0100

    keep going

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,14 @@
 import org.apache.commons.io.FileUtils
 import java.io.File
+import java.net.URI
 import java.net.URL
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 
 fun main() {
-    downloadEnvFile()
+    callGithubApi()
 }
 
 fun downloadEnvFile() {
@@ -12,4 +16,16 @@ fun downloadEnvFile() {
         URL("https://raw.githubusercontent.com/icaroland/cli-installer/main/src/unix/envs.sh"),
         File("envs.sh")
     )
+}
+
+fun callGithubApi() {
+    val request = HttpRequest.newBuilder()
+        .uri(URI("https://api.github.com/repos/icaroland/cli-entrypoint/tags"))
+        .GET()
+        .build()
+
+    val response =
+        HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString()).body()
+
+    print(response)
 }

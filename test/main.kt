940641f7cad369fbc8d33b1541dca0e2763cb5b8
LGala <galafassilorenzo@gmail.com>
2022-12-03 11:27:06 +0100

    keep going

--- test/main.kt
+++ test/main.kt
@@ -1,11 +1,14 @@
+import org.apache.commons.io.FileUtils
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.net.URL
 
 class DownloadTest {
     @Test
     fun shouldCorrectlyInstallIcaro() {
-        File("$HOME/.zshrc").createNewFile()
-
-        main()
+        FileUtils.copyURLToFile(
+            URL("https://github.com/icaroland/cli-entrypoint/releases/latest/download/entrypoint.jar"),
+            File("$HOME/icaro/cli/entrypoint.jar")
+        )
     }
 }

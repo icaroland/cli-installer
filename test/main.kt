35eba42db58ec12377f82feec250e3d08a6f499d
LGala <galafassilorenzo@gmail.com>
2022-12-03 11:28:22 +0100

    keep going

--- test/main.kt
+++ test/main.kt
@@ -6,9 +6,13 @@ import java.net.URL
 class DownloadTest {
     @Test
     fun shouldCorrectlyInstallIcaro() {
+        File("$HOME/icaro/cli/core").mkdirs()
+
         FileUtils.copyURLToFile(
             URL("https://github.com/icaroland/cli-entrypoint/releases/latest/download/entrypoint.jar"),
             File("$HOME/icaro/cli/entrypoint.jar")
         )
+
+        assert(File("$HOME/icaro/cli/entrypoint.jar").exists())
     }
 }

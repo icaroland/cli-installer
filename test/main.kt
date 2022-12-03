e1d83388a8d2213dd0143410932ca6ec80720a0e
LGala <galafassilorenzo@gmail.com>
2022-12-03 11:23:00 +0100

    keep going

--- test/main.kt
+++ test/main.kt
@@ -1,14 +1,8 @@
-/*
 import org.junit.jupiter.api.Test
-import java.io.File
 
 class DownloadTest {
     @Test
-    fun shouldCorrectlyDownloadFile() {
-        downloadEnvFile()
-
-        assert(File("envs.sh").exists())
-
-        File("envs.sh").delete()
+    fun shouldCorrectlyInstallIcaro() {
+        main()
     }
-}*/
+}

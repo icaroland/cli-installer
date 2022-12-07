eef138155e6f6c61671daeb062e969acede60877
LGala <galafassilorenzo@gmail.com>
2022-12-07 23:17:04 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -5,4 +5,4 @@
 #    $OutlookAccounts | Sort-Object -Property Size_MB -Descending | out-file c:\test.txt;
 #}
 
-(Get-Command java | Select-Object -ExpandProperty Version).toString()
+(Get-Command java | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major).toString()

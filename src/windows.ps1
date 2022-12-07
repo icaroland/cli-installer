ffca4019fa8e5959737c520d607ccb48638cff49
LGala <galafassilorenzo@gmail.com>
2022-12-07 23:40:41 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -1,6 +1,6 @@
 "starting to install Icaro!"
 
-$JavaVersion = [int](Get-Command java | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major)
+$JavaVersion = [int](Get-Command jav | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major)
 
 If ($JavaVersion -eq $null)
 {

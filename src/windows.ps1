dc81c87ca77907daf4acbd7f374b3ee72b28ff20
LGala <galafassilorenzo@gmail.com>
2022-12-08 00:40:56 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -19,3 +19,5 @@ if ($JavaVersion -lt 11)
 Get-ChildItem -Path ~/icaro -Recurse | Remove-Item -force -recurse
 Remove-Item ~/icaro -ErrorAction SilentlyContinue
 New-Item -Path "~\" -Name "icaro" -ItemType "directory"
+
+Get-ChildItem -Force -LiteralPath ~

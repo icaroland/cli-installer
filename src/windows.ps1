b8ae8a713fe42db3dae4b0a709c2d89e733f2a72
LGala <galafassilorenzo@gmail.com>
2022-12-07 23:38:04 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -2,9 +2,14 @@
 
 $JavaVersion = [int](Get-Command java | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major)
 
-If ($JavaVersion -e $null)
+If ($JavaVersion -eq $null)
 {
     throw "java 11+ is required to install Icaro!"
+}
+
+if ($JavaVersion -lt 11)
+{
+    throw "Error: the java version in use should be 11+, current: $JavaVersion"
 }
 
 $JavaVersion

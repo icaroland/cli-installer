e54c9fcfe70b28572b7f81f9b060e8eaefbe81c1
LGala <galafassilorenzo@gmail.com>
2022-12-08 02:50:52 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -21,8 +21,8 @@
 #New-Item -Path "~/" -Name "icaro" -ItemType "directory"
 #
 new-item -type file -path $profile -force
-[System.Environment]::SetEnvironmentVariable('ICARO_HOME', '~/icaro', [System.EnvironmentVariableTarget]::Machine)
-. $profile
+#[System.Environment]::SetEnvironmentVariable('ICARO_HOME', '~/icaro', [System.EnvironmentVariableTarget]::Machine)
+#. $profile
 ##$Env:ICARO_HOME = "~/icaro"
 ##$Env:ICARO_HOME
 

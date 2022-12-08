710095a78fdcc163ea83ecc00af487f8868818f6
LGala <galafassilorenzo@gmail.com>
2022-12-08 23:00:04 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -1,29 +1,32 @@
-#"starting to install Icaro!"
-#
-#$JavaVersion = 0
-#
-#try
-#{
-#    $JavaVersion = [int](Get-Command java | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major)
-#}
-#catch
-#{
-#    throw "java 11+ is required to install Icaro!"
-#}
-#
-#if ($JavaVersion -lt 11)
-#{
-#    throw "Error: the java version in use should be 11+, current: $JavaVersion"
-#}
-#
-#Get-ChildItem -Path ~/icaro -Recurse | Remove-Item -force -recurse
-#Remove-Item ~/icaro -ErrorAction SilentlyContinue
-#New-Item -Path "~/" -Name "icaro" -ItemType "directory"
-#
-new-item -type file -path $profile -force
-#[System.Environment]::SetEnvironmentVariable('ICARO_HOME', '~/icaro', [System.EnvironmentVariableTarget]::Machine)
-#. $profile
-##$Env:ICARO_HOME = "~/icaro"
-##$Env:ICARO_HOME
+"starting to install Icaro!"
 
-Get-Content -Path $profile
+$JavaVersion = 0
+
+try
+{
+    $JavaVersion = [int](Get-Command java | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major)
+}
+catch
+{
+    throw "java 11+ is required to install Icaro!"
+}
+
+if ($JavaVersion -lt 11)
+{
+    throw "Error: the java version in use should be 11+, current: $JavaVersion"
+}
+
+Get-ChildItem -Path ~/icaro -Recurse | Remove-Item -force -recurse
+Remove-Item ~/icaro -ErrorAction SilentlyContinue
+New-Item -Path "~/" -Name "icaro" -ItemType "directory"
+
+$profilePath = $profile.CurrentUserAllHosts
+if (!(Test-Path $profilePath))
+{
+    New-Item -ItemType File -Path $profilePath -Force
+}
+Add-Content -Path $profilePath -Value "$env:ICARO_HOME = '~icaro'"
+Add-Content -Path $profilePath -Value "New-Alias -Name icaro -Value java -jar ~/icaro/cli/entrypoint.jar"
+
+Get-ChildItem env:ICARO_HOME
+Get-Alias -Name icaro

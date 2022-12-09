007eaddc80d608abdba665eb59cba65f11a8e2f7
LGala <galafassilorenzo@gmail.com>
2022-12-09 13:24:48 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -1,41 +1,41 @@
-#"starting to install Icaro!"
-#
-#$JavaVersion = 0
-#
-#try
-#{
-#    $JavaVersion = [int](Get-Command java | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major)
-#}
-#catch
-#{
-#    throw "java 11+ is required to install Icaro!"
-#}
-#
-#if ($JavaVersion -lt 11)
-#{
-#    throw "Error: the java version in use should be 11+, current: $JavaVersion"
-#}
-#
-#Get-ChildItem -Path ~/icaro -Recurse | Remove-Item -force -recurse
-#Remove-Item ~/icaro -ErrorAction SilentlyContinue
-#New-Item -Path '~/icaro/lang' -ItemType Directory
-#New-Item -Path '~/icaro/cli/core' -ItemType Directory
-#
-#$profilePath = $profile.CurrentUserAllHosts
-#if (!(Test-Path $profilePath))
-#{
-#    New-Item -ItemType File -Path $profilePath -Force
-#}
-#Add-Content -Path $profilePath -Value "`$env:ICARO_HOME = `"~/icaro`""
-#Add-Content -Path $profilePath -Value "New-Alias -Name `'icaro`' -Value `'java -jar ~/icaro/cli/entrypoint.jar`'"
-#
-#. $profilePath
-#
-#Invoke-WebRequest -Verbose -Uri "https://github.com/icaroland/cli-entrypoint/releases/latest/download/entrypoint.jar" -OutFile "~/icaro/cli/entrypoint.jar"
-#
-#$lastCliCoreVersion = (Invoke-WebRequest -Uri "https://github.com/icaroland/cli-core/releases/latest" -ErrorAction:SilentlyContinue).Headers.Location
-#Invoke-WebRequest -Verbose -Uri "https://github.com/icaroland/cli-core/releases/download/$lastCliCoreVersion/$lastCliCoreVersion.jar" -OutFile "~/icaro/cli/core/$lastCliCoreVersion.jar"
-#
-#Get-ChildItem -Path '~/icaro' -Recurse | Format-List -Property FullName
+"starting to install Icaro!"
 
-(Invoke-WebRequest -Method HEAD -Uri "https://github.com/icaroland/cli-core/releases/latest" -MaximumRedirection 0 -ErrorAction:SilentlyContinue).Headers
+$JavaVersion = 0
+
+try
+{
+    $JavaVersion = [int](Get-Command java | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major)
+}
+catch
+{
+    throw "java 11+ is required to install Icaro!"
+}
+
+if ($JavaVersion -lt 11)
+{
+    throw "Error: the java version in use should be 11+, current: $JavaVersion"
+}
+
+Get-ChildItem -Path ~/icaro -Recurse | Remove-Item -force -recurse
+Remove-Item ~/icaro -ErrorAction SilentlyContinue
+New-Item -Path '~/icaro/lang' -ItemType Directory
+New-Item -Path '~/icaro/cli/core' -ItemType Directory
+
+$profilePath = $profile.CurrentUserAllHosts
+if (!(Test-Path $profilePath))
+{
+    New-Item -ItemType File -Path $profilePath -Force
+}
+Add-Content -Path $profilePath -Value "`$env:ICARO_HOME = `"~/icaro`""
+Add-Content -Path $profilePath -Value "New-Alias -Name `'icaro`' -Value `'java -jar ~/icaro/cli/entrypoint.jar`'"
+
+. $profilePath
+
+Invoke-WebRequest -Verbose -Uri "https://github.com/icaroland/cli-entrypoint/releases/latest/download/entrypoint.jar" -OutFile "~/icaro/cli/entrypoint.jar"
+
+$lastCliCoreVersion = (Invoke-WebRequest -Uri "https://github.com/icaroland/cli-core/releases/latest" -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction:SilentlyContinue).Headers.Location
+#Invoke-WebRequest -Verbose -Uri "https://github.com/icaroland/cli-core/releases/download/`$lastCliCoreVersion/$lastCliCoreVersion.jar" -OutFile "~/icaro/cli/core/$lastCliCoreVersion.jar"
+
+$lastCliCoreVersion
+
+Get-ChildItem -Path '~/icaro' -Recurse | Format-List -Property FullName

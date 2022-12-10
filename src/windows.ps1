652dd23bee35f58fcad82782b60b77eaf9bfe1af
LGala <galafassilorenzo@gmail.com>
2022-12-10 01:59:41 +0100

    keep going

--- src/windows.ps1
+++ src/windows.ps1
@@ -19,7 +19,7 @@
 #Get-ChildItem -Path ~/icaro -Recurse | Remove-Item -force -recurse
 #Remove-Item ~/icaro -ErrorAction SilentlyContinue
 New-Item -Path '~/icaro/lang' -ItemType Directory
-#New-Item -Path '~/icaro/cli/core' -ItemType Directory
+New-Item -Path '~/icaro/cli/core' -ItemType Directory
 #
 #$profilePath = $profile.CurrentUserAllHosts
 #if (!(Test-Path $profilePath))
